359884a3d5ee471b8b39a42c05b846132ff1eac8
kaybekov <kayumbekow@gmail.com>
2022-12-23 19:39:14 +0100

    css class addition

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -5,7 +5,13 @@ import data from './data';
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
-  return <h2>slider project setup</h2>;
+  return <section className='section'>
+    <div className="title">
+      <h2>
+        <span>/</span>reviews
+      </h2>
+    </div>
+  </section>;
 }
 
 export default App;
