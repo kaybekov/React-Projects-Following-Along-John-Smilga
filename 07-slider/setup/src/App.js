ffd2c0e1256e823a1f6c52ec0d3f40330f3cd495
kaybekov <kayumbekow@gmail.com>
2022-12-23 19:36:56 +0100

    use State addition

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -3,6 +3,8 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
   return <h2>slider project setup</h2>;
 }
 
